99d3e88c54c8bb4b87deb5a0e7bb06464ff54711
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-10-21 22:52:28 -0400

    add tree and runs the documentation example

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Cube from "./Cube";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
-      <h1>Pedro Ad-Cube Pet Project</h1>
+      <Cube />
     </div>
   );
 }
