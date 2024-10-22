f1f904e770626bf97a9aa6e19394c9eb8d494131
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-10-22 00:04:47 -0400

    add link to repo

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,14 @@ function App() {
   return (
     <div>
       <Cube />
+
+      <a
+        href="https://github.com/PedroMarianoAlmeida/ad-cube"
+        target="__blank"
+        rel="noopener noreferrer"
+      >
+        See code
+      </a>
     </div>
   );
 }
